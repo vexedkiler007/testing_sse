7c2c762a29aae0a54b387a7ea7183aa7239516da
Vexed <vexedkiller007@gmail.com>
2020-08-02 06:48:10 -0700

    added a bunch of stuff

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
-
+import json
+import sqlalchemy
 from flask import Flask, Response, render_template
-import sqlite3
 import time
 app = Flask(__name__)
 
 
 def get_data():
-    time.sleep(1)
-    s = time.ctime(time.time())
-    return s
+    engine = sqlalchemy.create_engine("mysql+mysqldb://root@localhost/cool_rules")
+    with engine.connect() as connection:
+        result = connection.execute("SELECT * FROM Rules")
+        time.sleep(10)
+    return json.dumps([(row['author'], row['rule']) for row in result])
 
 
 @app.route('/')
 def root():
-    return render_template('client_app.html')
+    engine = sqlalchemy.create_engine("mysql+mysqldb://root@localhost/cool_rules")
+    with engine.connect() as connection:
+        result = connection.execute("SELECT * FROM Rules")
+    result_ = [(row['author'], row['rule']) for row in result]
+    print(len(result_))
+    return render_template('client_app.html', result= result_, result_len=len(result_))
 
 
 @app.route("/stream")
 def stream():
+
     def eventStream():
         while True:
             yield f'data:{get_data()}\n\n'
 
     return Response(eventStream(), mimetype="text/event-stream")
+
 
 if __name__ == '__main__':
     app.run()
--- test_databse.py
+++ test_databse.py
@@ -1,4 +1,7 @@
 import sqlite3
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, text
 
 
 def create_rules_table(name):
@@ -10,10 +13,32 @@ def create_rules_table(name):
 def insert_data():
     conn = sqlite3.connect("test.sqlite")
     cur = conn.cursor()
-    cur.execute("""INSERT INTO Rules (id, author, rule) values("1", "David", "This is cool rule")""")
+    cur.execute("""INSERT INTO Rules (id, author, rule) values("2", "Sarah", "This is cool rule")""")
     conn.commit()
 
 
-create_rules_table('test')
-for _ in range(0, 10):
-    insert_data()
+def test_sqlite3():
+    create_rules_table('test')
+    for _ in range(0, 10):
+        insert_data()
+
+
+engine = sqlalchemy.create_engine("mysql+mysqldb://root:@localhost/cool_rules")
+Base = declarative_base()
+
+
+class Rules(Base):
+    __tablename__ = 'Rules'
+
+    id = Column(Integer, primary_key=True)
+    author = Column(String(256))
+    rule = Column(String(256))
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
+            self.id, self.author, self.rule)
+
+
+Base.metadata.create_all(engine)
+
+
